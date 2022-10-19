baf3b07bea3d5b53400d314541dd52c05b0697cd
Rahat Zaman <rahatzamancse@gmail.com>
2022-10-19 10:25:45 -0700

    Fixed Navbar
    - Minimized the space of navbar so we can use more space for actual stuff
    - Change color to grayscale
    - Replaced plain-old text navbar with bootstrap. Improves overall maintainability
    - Moved all the graphic overview links to a single dropdown.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,63 @@
 import React from "react-dom"
 import { NavLink, Outlet } from "react-router-dom"
-import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function  isActiveNavLink({  isActive }) {
+
+// For navigation bar
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function isActiveNavLink({ isActive }) {
   return {
-    textDecoration: isActive?"underline":"none",
-    fontWeight: isActive?"bold":"normal"
+    textDecoration: isActive ? "underline" : "none",
+    fontWeight: isActive ? "bold" : "normal"
   }
 }
 
 export default function App() {
   return (
-	<div>
-    <div className="header">
-      <h1>Explore Biomedical Literature</h1>
-      <nav>
-          <NavLink to="/" style={isActiveNavLink}>Interactions Overview</NavLink> |{" "}
-          <NavLink to="/blob-viz-il6" style={isActiveNavLink}>Graphic Overview (IL-6)</NavLink> | {" "}
-          <NavLink to="/blob-viz-tnf-fat" style={isActiveNavLink}>Graphic Overview (TNF-FAT)</NavLink> | {" "}
-          {/* <NavLink to="/viz" style={isActiveNavLink}>Network View</NavLink> | {" "} */}
-          <NavLink to="/evidence-index" style={isActiveNavLink}>Search Evidence</NavLink> | {" "}
-          <NavLink to="/structured-search" style={isActiveNavLink}>Structured Evidence Search</NavLink>
-        </nav>
+    <div>
+      <Navbar bg="dark" variant="dark" className="me-auto" expand="lg" style={{
+        paddingLeft: "2em",
+        paddingRight: "2em",
+      }}>
+        <Container fluid>
+          <LinkContainer to="/">
+            <Navbar.Brand>Explore Biomedical Literature</Navbar.Brand>
+          </LinkContainer>
+            <Nav>
+              <LinkContainer to="/">
+                  <Nav.Link>Interactions Overview</Nav.Link>
+              </LinkContainer>
+
+              <NavDropdown title="Graphic Overview" id="basic-nav-dropdown">
+              <LinkContainer to="/blob-viz-il6">
+                  <NavDropdown.Item>Start from IL-6</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/blob-viz-tnf-fat">
+                  <NavDropdown.Item>Start from TNF-FAT</NavDropdown.Item>
+              </LinkContainer>
+              </NavDropdown>
+
+
+              <LinkContainer to="/evidence-index">
+                  <Nav.Link>Search Evidence</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/structured-search">
+                  <Nav.Link>Structured Evidence Search</Nav.Link>
+              </LinkContainer>
+            </Nav>
+        </Container>
+      </Navbar>
+      <div style={{
+        margin: "0 1em 1em 1em"
+      }}>
+        <Outlet />
+      </div>
     </div>
-	  <br />
-    <div className="content">
-      <Outlet />
-    </div>
-	</div>
   )
 }
